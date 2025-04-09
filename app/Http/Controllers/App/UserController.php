dbe5e9517ab19ddcff1cf091b292a7e5083baa35
Junry Lumapac <2201106837@student.buksu.edu.ph>
2025-04-10 01:15:57 +0800

    add tenant user permission role with edit

--- app/Http/Controllers/App/UserController.php
+++ app/Http/Controllers/App/UserController.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\App;
-
-use App\Models\Tenant;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Spatie\Permission\Models\Role as SpatieRole;
 
 class UserController extends Controller
 {
@@ -48,7 +48,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Tenant $tenant)
+    public function show(User $user)
     {
         //
     }
@@ -56,23 +56,36 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Tenant $tenant)
+    public function edit(User $user)
     {
-        //
+        $roles = SpatieRole::get();
+        // dd($users->toArray());
+        return view('app.users.edit', compact('user','roles'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Tenant $tenant)
+    public function update(Request $request, User $user)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $user->id,
+            'roles' => 'required|array',
+
+        ]);
+        // dd($validatedData);
+        $user->update($validatedData);
+        $user->roles()->sync($request->input('roles'));
+
+        return redirect()->route('users.index')->with('success', 'User created successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tenant $tenant)
+    public function destroy(User $user)
     {
         //
     }
--- resources/views/app/layouts/navigation.blade.php
+++ resources/views/app/layouts/navigation.blade.php
@@ -17,12 +17,13 @@
                     </x-nav-link>
                 </div>
 
+                @role('admin')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('addUser')">
                         {{ __('User') }}
                     </x-nav-link>
                 </div>
-
+                @endrole
             </div>
 
             <!-- Settings Dropdown -->
